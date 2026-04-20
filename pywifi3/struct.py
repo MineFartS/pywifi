29f16b2204949a804a8e0fb42d72793bc6dcea24
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-20 17:17:53 -0400

    Update struct.py

--- pywifi3/struct.py
+++ pywifi3/struct.py
@@ -6,6 +6,35 @@ from ctypes import (
 from ctypes.wintypes import DWORD, HANDLE
 from comtypes import GUID
 
+# Define interface status.
+IFACE_DISCONNECTED = 0
+IFACE_SCANNING = 1
+IFACE_INACTIVE = 2
+IFACE_CONNECTING = 3
+IFACE_CONNECTED = 4
+
+# Define auth algorithms.
+AUTH_ALG_OPEN = 0
+AUTH_ALG_SHARED = 1
+
+# Define auth key mgmt types.
+AKM_TYPE_NONE = 0
+AKM_TYPE_WPA = 1
+AKM_TYPE_WPAPSK = 2
+AKM_TYPE_WPA2 = 3
+AKM_TYPE_WPA2PSK = 4
+AKM_TYPE_UNKNOWN = 5
+
+# Define ciphers.
+CIPHER_TYPE_NONE = 0
+CIPHER_TYPE_WEP = 1
+CIPHER_TYPE_TKIP = 2
+CIPHER_TYPE_CCMP = 3
+CIPHER_TYPE_UNKNOWN = 4
+
+KEY_TYPE_NETWORKKEY = 0
+KEY_TYPE_PASSPHRASE = 1
+
 handle = HANDLE()
 
 WLAN_MAX_PHY_TYPE_NUMBER = 8
