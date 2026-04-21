717d78891888b56656872914a0951b2718936b1b
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-20 21:50:45 -0400

    Update profile.py

--- pywifi2/profile.py
+++ pywifi2/profile.py
@@ -1,4 +1,5 @@
 from pywifi.profile import Profile as __Profile
+from philh_myftp_biz.num import clamp
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -16,14 +17,14 @@ class Profile(__Profile):
         super().__init__()
         
         self.id: int = profile.id
-        self.auth = profile.auth
-        self.akm = profile.akm
-        self.cipher = profile.cipher
+        self.auth: int = profile.auth
+        self.akm: list[int] = profile.akm
+        self.cipher: int = profile.cipher
         self.ssid: None|str = profile.ssid
         self.bssid = profile.bssid
         self.key = profile.key
         self.signal = profile.signal
         self.freq = profile.freq
 
-        _p = (2 * (profile.signal + 100))
-        self.percent: int = max(min(_p, 100), 0)
+        self.percent: int|float = clamp((2*(profile.signal + 100)), 0, 100)
+
